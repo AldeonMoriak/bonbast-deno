2440a7cc41be148c059dea40ed9c096515320386
Mehran Mishekaran <aldeonmoriak@gmail.com>
2024-10-26 13:18:32 +0330

    add coins to the list

--- bonbast.ts
+++ bonbast.ts
@@ -3,57 +3,66 @@ import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 const BASE_URL = "https://bonbast.com";
 
 const headers = new Headers({
-  "Host": "bonbast.com",
+  Host: "bonbast.com",
   "User-Agent":
     "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0",
-  "Accept": "application/json, text/javascript, */*; q=0.01",
+  Accept: "application/json, text/javascript, */*; q=0.01",
   "Accept-Language": "en-US,en;q=0.5",
   "Accept-Encoding": "gzip, deflate, br",
   "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
   "X-Requested-With": "XMLHttpRequest",
   "Content-Length": "68",
-  "Origin": "https://bonbast.com",
-  "Connection": "keep-alive",
-  "Referer": "https://bonbast.com/",
-  "Cookie": "cookieconsent_status=true; st_bb=0",
+  Origin: "https://bonbast.com",
+  Connection: "keep-alive",
+  Referer: "https://bonbast.com/",
+  Cookie: "cookieconsent_status=true; st_bb=0",
   "Sec-Fetch-Dest": "empty",
   "Sec-Fetch-Mode": "cors",
   "Sec-Fetch-Site": "same-origin",
-  "TE": "trailers",
+  TE: "trailers",
 });
 
 const CURRENCIES = {
-  "usd": "US Dollar",
-  "eur": "Euro",
-  "gbp": "British Pound",
-  "chf": "Swiss Franc",
-  "cad": "Canadian Dollar",
-  "aud": "Australian Dollar",
-  "sek": "Swedish Krona",
-  "nok": "Norwegian Krone",
-  "rub": "Russian Ruble",
-  "thb": "Thai Baht",
-  "sgd": "Singapore Dollar",
-  "hkd": "Hong Kong Dollar",
-  "azn": "Azerbaijani Manat",
-  "amd": "10 Armenian Dram",
-  "dkk": "Danish Krone",
-  "aed": "UAE Dirham",
-  "jpy": "10 Japanese Yen",
-  "try": "Turkish Lira",
-  "cny": "Chinese Yuan",
-  "sar": "Saudi Riyal",
-  "inr": "Indian Rupee",
-  "myr": "Malaysian Ringgit",
-  "afn": "Afghan Afghani",
-  "kwd": "Kuwaiti Dinar",
-  "iqd": "100 Iraqi Dinar",
-  "bhd": "Bahraini Dinar",
-  "omr": "Omani Rial",
-  "qar": "Qatari Rial",
-};
+  usd: "US Dollar",
+  eur: "Euro",
+  gbp: "British Pound",
+  chf: "Swiss Franc",
+  cad: "Canadian Dollar",
+  aud: "Australian Dollar",
+  sek: "Swedish Krona",
+  nok: "Norwegian Krone",
+  rub: "Russian Ruble",
+  thb: "Thai Baht",
+  sgd: "Singapore Dollar",
+  hkd: "Hong Kong Dollar",
+  azn: "Azerbaijani Manat",
+  amd: "10 Armenian Dram",
+  dkk: "Danish Krone",
+  aed: "UAE Dirham",
+  jpy: "10 Japanese Yen",
+  try: "Turkish Lira",
+  cny: "Chinese Yuan",
+  sar: "Saudi Riyal",
+  inr: "Indian Rupee",
+  myr: "Malaysian Ringgit",
+  afn: "Afghan Afghani",
+  kwd: "Kuwaiti Dinar",
+  iqd: "100 Iraqi Dinar",
+  bhd: "Bahraini Dinar",
+  omr: "Omani Rial",
+  qar: "Qatari Rial",
+} as const;
+
+const COINS = {
+  azadi1: "Azadi",
+  emami1: "Emami",
+  azadi1_2: "½ Azadi",
+  azadi1_4: "¼ Azadi",
+  azadi1g: "Gerami",
+} as const;
 
 type CurrencyType = keyof typeof CURRENCIES;
+type CoinType = keyof typeof COINS;
 const SELL = 1;
 const BUY = 2;
 
@@ -61,8 +70,10 @@ async function getToken(): Promise<string | undefined> {
   try {
     const res = await fetch(BASE_URL, { headers });
     const html = await res.text();
-    const document = new DOMParser().parseFromString(html, "text/html")
-      ?.textContent;
+    const document = new DOMParser().parseFromString(
+      html,
+      "text/html",
+    )?.textContent;
     const extraction = document?.match(/param\s*[=:]\s*\"(.+)\"/);
     return extraction![1];
   } catch (error) {
@@ -70,8 +81,20 @@ async function getToken(): Promise<string | undefined> {
   }
 }
 
-export async function getCurrencies(): Promise<
-  { currency: string; sell: string; buy: string }[] | undefined
+export type CurrencyListType = {
+  currency: (typeof CURRENCIES)[CurrencyType];
+  buy: string;
+  sell: string;
+};
+
+export type CoinListType = {
+  coin: (typeof COINS)[CoinType];
+  buy: string;
+  sell: string;
+};
+
+export async function getCurrenciesAndCoins(): Promise<
+  { currencyList: CurrencyListType[]; coinList: CoinListType[] } | undefined
 > {
   try {
     const token = await getToken();
@@ -82,25 +105,45 @@ export async function getCurrencies(): Promise<
         method: "post",
         body,
       });
-      return fetch(request).then((response) => {
-        return response.json();
-      }).then((data) => {
-        const currencyList = [];
+      return fetch(request)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data: Record<string, string>) => {
+          const currencyList: CurrencyListType[] = [];
 
-        for (const currency of Object.keys(CURRENCIES)) {
-          const sellValue = data[`${currency}${SELL}` as CurrencyType]
-            .toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-          const buyValue = data[`${currency}${BUY}` as CurrencyType].toString()
-            .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-          const currentCurrency = {
-            "currency": CURRENCIES[currency as CurrencyType],
-            buy: buyValue,
-            sell: sellValue,
-          };
-          currencyList.push(currentCurrency);
-        }
-        return currencyList;
-      });
+          const coinList: CoinListType[] = [];
+
+          for (const currency of Object.keys(CURRENCIES)) {
+            const sellValue = data[`${currency}${SELL}` as CurrencyType]
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            const buyValue = data[`${currency}${BUY}` as CurrencyType]
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            const currentCurrency = {
+              currency: CURRENCIES[currency as CurrencyType],
+              buy: buyValue,
+              sell: sellValue,
+            };
+            currencyList.push(currentCurrency);
+          }
+          for (const coin of Object.keys(COINS)) {
+            const sellValue = data[`${coin}`]
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            const buyValue = data[`${coin}${BUY}`]
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            const currentCoin = {
+              coin: COINS[coin as CoinType],
+              buy: buyValue,
+              sell: sellValue,
+            };
+            coinList.push(currentCoin);
+          }
+          return { currencyList, coinList };
+        });
     }
   } catch (error) {
     console.error(error);
--- index.ts
+++ index.ts
@@ -6,28 +6,32 @@ import {
   Bot,
   webhookCallback,
 } from "https://deno.land/x/grammy@v1.17.2/mod.ts";
-import { getCurrencies } from "./bonbast.ts";
+import {
+  CoinListType,
+  CurrencyListType,
+  getCurrenciesAndCoins,
+} from "./bonbast.ts";
 
 const bot = new Bot(Deno.env.get("TELEGRAM_KEY") || "");
 
-bot.command(
-  "start",
-  (ctx) => ctx.reply("Hello. get the latest currencies in Toman"),
+bot.command("start", (ctx) =>
+  ctx.reply("Hello. get the latest currencies in Toman"),
 );
 bot.command("get", async (ctx) => {
   const user = await ctx.getAuthor();
-  if (
-    !Deno.env.get("USERS")!.split(",").includes(
-      user.user.username!,
-    )
-  ) {
+  if (!Deno.env.get("USERS")!.split(",").includes(user.user.username!)) {
     console.log("*******", user.user.username, "*****", Deno.env.get("USERS"));
     return ctx.reply("Scram! You're not allowed to use this bot.");
   }
   try {
-    const currencies = await getCurrencies();
-    if (currencies) {
-      return ctx.reply(makeTable(currencies), { parse_mode: "MarkdownV2" });
+    const result = await getCurrenciesAndCoins();
+    if (result?.currencyList && result?.coinList) {
+      return ctx.reply(
+        makeCurrencyTable(result.currencyList).concat(
+          makeCoinTable(result.coinList),
+        ),
+        { parse_mode: "MarkdownV2" },
+      );
     }
     return ctx.reply("Something went wrong!");
   } catch (error) {
@@ -54,28 +58,49 @@ Deno.serve(async (req) => {
 });
 
 function rightpad(text: string, length: number): string {
-  return text.concat(
-    Array.from({ length: length - text.length }).join(" "),
+  return text.concat(Array.from({ length: length - text.length }).join(" "));
+}
+
+function makeCurrencyTable(currencies: CurrencyListType[]): string {
+  const data = currencies
+    .map(
+      (item) =>
+        `| ${rightpad(item.currency, 20)} | ${rightpad(item.buy, 10)} | ${rightpad(
+          item.sell,
+          10,
+        )} | \n`,
+    )
+    .join("");
+  return (
+    "```\n" +
+    `| ${rightpad("Currency", 20)} | ${rightpad("Buy", 10)} | ${rightpad(
+      "Sell",
+      10,
+    )} |\n| :------------------ | --------- | --------- |\n${data}\n`
+      .replace(/\|/g, "\\|")
+      .replace(/\-/g, "\\-") +
+    "```"
   );
 }
 
-function makeTable(
-  currencies: { currency: string; buy: string; sell: string }[],
-): string {
-  const data = currencies.map(
-    (
-      item,
-    ) => (`| ${rightpad(item.currency, 20)} | ${rightpad(item.buy, 10)} | ${
-      rightpad(item.sell, 10)
-    } | \n`),
-  ).join("");
-  return "```\n" +
-    (`| ${rightpad("Currency", 20)} | ${rightpad("Buy", 10)} | ${
-      rightpad("Sell", 10)
-    } |\n| :------------------ | --------- | --------- |\n${data}\n`)
-      .replace(
-        /\|/g,
-        "\\|",
-      ).replace(/\-/g, "\\-") +
-    "```";
+function makeCoinTable(coins: CoinListType[]): string {
+  const data = coins
+    .map(
+      (item) =>
+        `| ${rightpad(item.coin, 20)} | ${rightpad(item.buy, 10)} | ${rightpad(
+          item.sell,
+          10,
+        )} | \n`,
+    )
+    .join("");
+  return (
+    "```\n" +
+    `| ${rightpad("Coin", 20)} | ${rightpad("Buy", 10)} | ${rightpad(
+      "Sell",
+      10,
+    )} |\n| :------------------ | --------- | --------- |\n${data}\n`
+      .replace(/\|/g, "\\|")
+      .replace(/\-/g, "\\-") +
+    "```"
+  );
 }
